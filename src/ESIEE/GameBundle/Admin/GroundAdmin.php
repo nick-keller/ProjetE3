43bc6c50c3759deab408b599a2d465ea3770be60
nick-keller <nicolas.keller@vimies.com>
2014-05-01 10:58:42 +0200

    Created Ground admin

---
+++ src/ESIEE/GameBundle/Admin/GroundAdmin.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace ESIEE\GameBundle\Admin;
+
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+
+class GroundAdmin extends Admin
+{
+    protected function configureShowFields(ShowMapper $filter)
+    {
+        $filter
+            ->add('id')
+            ->add('name')
+        ;
+    }
+
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name', 'text', array(
+                'label' => 'Nom'
+            ))
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('id')
+            ->addIdentifier('name')
+            ->add('_action', 'actions', array(
+                'actions' => array(
+                    'show' => array(),
+                    'edit' => array(),
+                    'delete' => array(),
+                )
+            ))
+        ;
+    }
+}
